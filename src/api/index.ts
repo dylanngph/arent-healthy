9003012f34091d9a29583b114fd734f90072b4e7
Duy Nguyen <duyngph@gmail.com>
2023-08-02 17:49:00 +0700

    added api url

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const apiUrl = import.meta.env.VITE_API_URL ?? 'http://localhost:3001';
+const apiUrl = import.meta.env.VITE_API_URL ?? 'https://arent-test-api.onrender.com';
 
 export const axiosInstance = axios.create({
     baseURL: apiUrl,
